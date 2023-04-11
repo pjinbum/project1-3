cbce3291d110b6c0a24c5d4ea8c968d48cd65c41
pjinbum <onepja@naver.com.com>
2023-04-11 10:30:21 +0900

    ⚡️

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Gesifan from './Box4/Gesifan'
+import Cctv from './Box5/Cctv'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Gesifan></Gesifan>
+      <Cctv></Cctv>
+    </div>
+  );
+}
+
+export default App;
